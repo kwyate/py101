d5280eb0940c660c740fbab5694822a3c839399d
Kewuin Yate <48801273+kwyate@users.noreply.github.com>
2019-10-23 20:30:42 -0500

    Update main.py

--- Juego_21/main.py
+++ Juego_21/main.py
@@ -1,16 +1,16 @@
 from funciones import *
 
 def juego():
-    userP = 0
-    maqP = 0
-    jugar = "si"
+    userP = 0 # puntaje Partidas ganadas jugador
+    maqP = 0 # puntaje Partidas ganadas jugador
+    jugar = "si" # variable para iniciar juego
     while jugar == "si":
-        numTur = 1
-        numU = 0
-        numC = 0
+        numTur = 1 # Numero de turnos jugados
+        numU = 0 # puntaje de las cartas usuario
+        numC = 0 # puntaje de las cartas maquina
         while True:
             
-            if numTur == 1:
+            if numTur == 1: 
                 print("-----------||------------")
                 print("Turno "+ nombre + " :                        Turno MAQUINA: ")
                 print("Cartas "+ nombre + ": " + str(turno("user")) +"           Cartas MAQUINA: [" + str(turno("maquina")[0]) + ", --]")
@@ -18,12 +18,10 @@ def juego():
                 print("Resultado "+ nombre + " es: " +str(numU))
                 print("-----------||------------||||---------------||----------------")
                 print("-----------||------------||||---------------||----------------")
-                #print("Turno MAQUINA: ")
-                #print("Cartas maquina: " + str(turno("maquina")[0]) + ", --")
-                #print("Cartas MAQUINA: " + str(turno("maquina")))
+    
                 numC += suma(0, 0, "maquina")
                 numTur +=1
-                #print("Resultado MAQUINA es: " +str(numC))
+                
             else:
                 print("Turno No"+ str(numTur)+" "+ nombre + ": ")
                 numTur +=1
@@ -32,33 +30,44 @@ def juego():
                     print("-----------||------------")
                     print (turno("user"))
                     numU = suma(numU, len(user())-1, "user")
+                    numA = 0
+                    if ("Ad" in user() or "Ap" in user() or "At" in user() or  "Ac" in  user()) and numU > 21:
+                    
+                        for i in range(0, len(user())):
+                            if "A" in user()[i][0]:
+                                numA += 1
+                        
+                        numU-=numA*10
+                        print ("Se pudo restar " + str(numU))
+                        
                     print("Resultado "+ nombre + " es: " +str(numU))
                     print("-----------||------------")
                     if numU >21:
-                        break
+                       print("----pass-------| EL JUGADOR "+ nombre + " SE HA PLANTADO |------------")
+                       break
+                    
                 des2 = input("Deseas plantar tu juego si o no? : ")
                 if des2 == "si":
                     print("-----------| EL JUGADOR "+ nombre + " SE HA PLANTADO |------------")
                     break
         
         while True:
-            desC = random.choice(["Si", "No"])
-            if (desC == "Si" and numC < 21) or numC <=10:
-                print("-----------||------------")
-                print("otra carta para MAQUINA")
-                print("-----------||------------")
-                print(turno("maquina"))
-                numC = suma(numC, len(pc())-1, "maquina")
-                #print("longitud pc: " + str(len(pc())))
-            elif desC == "No" or numC == 21 or numC > 21:
+            if  numC == 21 or numC > 21 or numU > 21 and numC <= 21 or numC >= numU and numC < 21:
                 print("-----------||------------")
                 print("Cartas MAQUINA: " + str(pc()))
                 print("Resultado MAQUINA es: " +str(numC))
                 print("-----------| LA MAQUINA SE HA PLANTADO |------------")
                 break
+
+            else:
+                print("-----------||------------")
+                print("otra carta para MAQUINA")
+                print("-----------||------------")
+                print(turno("maquina"))
+                numC = suma(numC, len(pc())-1, "maquina")
+                
         userP, maqP = validarGanador(numU, numC, userP, maqP)
-        #userP += userP
-        #maqP += maqP
         print("El puntaje es: " + nombre + " con "+ str(userP) + "----||--- MAQUINA con " + str(maqP))
         jugar = input("Desea jugar de nuevo si o no ?")
 print (juego())
+
